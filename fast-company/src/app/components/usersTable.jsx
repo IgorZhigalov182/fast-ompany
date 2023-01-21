e079ecfe31508ce5b2c853c52b57a52aed9037b6
Igor <zhigalovzenit112233@gmail.com>
2023-01-21 17:37:08 +0300

    decomposition component user in tableBody added delete button & bookmark

--- fast-company/src/app/components/usersTable.jsx
+++ fast-company/src/app/components/usersTable.jsx
@@ -1,30 +1,58 @@
 import React from "react";
-import User from "./user";
+// import User from "./user";
 import PropTypes from "prop-types";
 import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
+import BookMark from "./bookmark";
 
-const UserTable = ({ users, selectedSort, onSort, ...rest }) => {
+const UserTable = ({
+    users,
+    selectedSort,
+    onSort,
+    onToggleBookMark,
+    onDelete,
+    ...rest
+}) => {
     const columns = {
-        name: { iter: "name", name: "Имя" },
+        name: { path: "name", name: "Имя" },
         qualities: { name: "Качества" },
-        professions: { iter: "profession.name", name: "Профессия" },
+        professions: { path: "profession.name", name: "Профессия" },
         completedMeetings: {
-            iter: "completedMeetings",
+            path: "completedMeetings",
             name: "Встретился, раз"
         },
-        rate: { iter: "rate", name: "Оценка" },
-        bookmark: { iter: "bookmark", name: "Избранное" },
-        delete: {}
+        rate: { path: "rate", name: "Оценка" },
+        bookmark: {
+            path: "bookmark",
+            name: "Избранное",
+            component: (user) => (
+                <BookMark
+                    status={user.bookmark}
+                    onClick={() => onToggleBookMark(user._id)}
+                />
+            )
+        },
+        delete: {
+            component: (user) => (
+                <button
+                    onClick={() => onDelete(user._id)}
+                    className="btn btn-danger"
+                >
+                    delete
+                </button>
+            )
+        }
     };
 
     return (
         <table className="table">
             <TableHeader {...{ onSort, selectedSort, columns }} />
-            <tbody>
+            <TableBody {...{ data: users, columns }} />
+            {/* <tbody>
                 {users.map((user) => (
                     <User {...rest} {...user} key={user._id} />
                 ))}
-            </tbody>
+            </tbody> */}
         </table>
     );
 };
@@ -32,7 +60,9 @@ const UserTable = ({ users, selectedSort, onSort, ...rest }) => {
 UserTable.propTypes = {
     users: PropTypes.array.isRequired,
     onSort: PropTypes.func.isRequired,
-    selectedSort: PropTypes.object.isRequired
+    selectedSort: PropTypes.object.isRequired,
+    onToggleBookMark: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired
 };
 
 export default UserTable;
