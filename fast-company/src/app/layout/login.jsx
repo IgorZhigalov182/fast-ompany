bde8579fec5bd42aa2f99b693c15f2b130dea32d
Igor <zhigalovzenit112233@gmail.com>
2023-02-06 22:39:15 +0300

    create forms in login

--- fast-company/src/app/layout/login.jsx
+++ fast-company/src/app/layout/login.jsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+    const [data, setData] = useState({ email: "", password: "" });
+
+    const handleEmail = (e) => {
+        setData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    };
+
     return (
         <>
-            <h1>Login</h1>
+            <form action="">
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="text"
+                        id="email"
+                        value={data.email}
+                        name="email"
+                        onChange={handleEmail}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={data.password}
+                        onChange={handleEmail}
+                    />
+                </div>
+                <div>
+                    <div>
+                        <label htmlFor="radio1">
+                            Radio 1 {""}
+                            <input type="radio" id="radio1" name="radio" />
+                        </label>
+                    </div>
+                    <div>
+                        <label htmlFor="radio2">
+                            Radio 2 {""}
+                            <input type="radio" id="radio2" name="radio" />
+                        </label>
+                    </div>
+                </div>
+            </form>
         </>
     );
 };
