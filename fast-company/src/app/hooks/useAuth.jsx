9c756b224a6500d9efb8eb5500175d80489712ec
Igor <zhigalovzenit112233@gmail.com>
2023-04-27 21:30:43 +0300

    create function to createUsers

--- fast-company/src/app/hooks/useAuth.jsx
+++ fast-company/src/app/hooks/useAuth.jsx
@@ -60,9 +60,11 @@ const AuthProvider = ({ children }) => {
         setUser(null);
         history.push("/");
     }
+
     function randomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
+
     async function updateUserData(data) {
         try {
             const { content } = await userService.update(data);
