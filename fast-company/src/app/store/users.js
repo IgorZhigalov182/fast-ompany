e261ea0188cb7e55097b9fdce77823a712817485
Igor <zhigalovzenit112233@gmail.com>
2023-04-29 17:11:15 +0300

    create comment service on redux

--- fast-company/src/app/store/users.js
+++ fast-company/src/app/store/users.js
@@ -4,6 +4,7 @@ import authService from "../services/auth.service";
 import localStorageService from "../services/localStorage.service";
 import getRandomInt from "../utils/getRandomInt";
 import history from "../utils/history";
+import { generateAuthError } from "../utils/generateAuthError";
 
 const initialState = localStorageService.getAccessToken()
     ? {
@@ -68,6 +69,9 @@ const usersSlice = createSlice({
             // const userId = action.payload._id;
             // console.log(state.entities[0]);
             // console.log(action.payload.users);
+        },
+        authRequested: (state) => {
+            state.error = null;
         }
     }
 });
@@ -101,7 +105,13 @@ export const login =
             localStorageService.setTokens(data);
             history.push(redirect);
         } catch (error) {
-            dispatch(authRequestFailed(error.message));
+            const { code, message } = error.response.data.error;
+            if (code === 400) {
+                const errorMessage = generateAuthError(message);
+                dispatch(authRequestFailed(errorMessage));
+            } else {
+                dispatch(authRequestFailed(error.message));
+            }
         }
     };
 
@@ -189,5 +199,5 @@ export const getIsLoggedIn = () => (state) => state.users.isLoggedIn;
 export const getDataStatus = () => (state) => state.users.dataLoaded;
 export const getUsersLoadingStatus = () => (state) => state.users.isLoading;
 export const getCurrentUserId = () => (state) => state.users.auth.userId;
-
+export const getAuthErrors = () => (state) => state.users.error;
 export default usersReducer;
